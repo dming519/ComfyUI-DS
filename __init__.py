86c4ce83fa4c5b8cacc13eb14c147f4c9d549944
liangdongming <398286600@qq.com>
2024-12-02 10:01:45 +0800

    feat(DS): 添加图像列表处理节点
    - 新增 AddImageToList 节点，用于将单张图片添加到图像列表中- 新增 ImageListToImage节点，用于将图像列表转换为单张图片输出
    - 新增 SliceImageList 节点，用于截取图像列表中的指定部分
    - 这些节点为图像列表的处理提供了更多可能性和便利性

--- __init__.py
+++ __init__.py
@@ -1,8 +1,17 @@
 from .ApiPost import ApiPost
+from .AddImageToList import AddImageToList
+from .SliceImageList import SliceImageList
+from .ImageListToImage import ImageListToImage
 
 NODE_CLASS_MAPPINGS = {
-    "DS_ApiPost": ApiPost
+    "DS_ApiPost": ApiPost,
+    "DS_AddImageToList": AddImageToList,
+    "DS_SliceImageList": SliceImageList,
+    "DS_ImageListToImage": ImageListToImage,
 }
 NODE_DISPLAY_NAME_MAPPINGS = {
-    "DS_ApiPost" : "DS🦥POST请求"
+    "DS_ApiPost" : "DS🦥POST请求",
+    "DS_AddImageToList": "DS🦥AddImageToList",
+    "DS_SliceImageList": "DS🦥SliceImageList",
+    "DS_ImageListToImage": "DS🦥ImageListToImage",
 }
